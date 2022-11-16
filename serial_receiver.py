b5cca5e458b668446ee5e213def87df280af8950
Daniel Gullberg <daniel_gullberg@hotmail.com>
2022-11-16 23:47:57 +0100

    Publishin the water meter data with zmq

--- serial_receiver.py
+++ serial_receiver.py
@@ -1,5 +1,6 @@
 """Receiving serial data from the NodeMCU connected to the water meter"""
 import serial
+import publisher
 
 class SerialReceiver:
     """Serial receiver class"""
@@ -9,20 +10,27 @@ class SerialReceiver:
 
     def _parseAndSendData(self,receivedRaw):
         """Parse serial data and send over TCP/IP"""
-        pulses, qVolumeLPerSStr, qVolumeM3PerHStr = receivedRaw.strip().split()
+        pulsesStr, qVolumeLPerSStr, qVolumeM3PerHStr = receivedRaw.strip().split()
+        pulses = int(pulsesStr)
         qVolumeLPerS = float(qVolumeLPerSStr)
         qVolumeM3PerH = float(qVolumeM3PerHStr)
         print(f"{pulses} {qVolumeLPerS} {qVolumeM3PerH}")
-        print(repr(receivedRaw))
 
+        dataObj = {'pulses': pulses,
+                   'qVolumeLPerS': qVolumeLPerS,
+                   'qVolumeM3PerH': qVolumeM3PerH}
+        self._publisher.send({'waterMeter': dataObj})
         
-    def connectAndRun(self, port):
+    def connectAndRun(self, port, zmqPort):
         """Connect to port and run receiver"""
         self._ser.baudrate = 115200
         self._ser.timeout = 1
         self._ser.port = port
         self._ser.open()
 
+        self._publisher = publisher.Publisher()
+        self._publisher.open(port=zmqPort)
+        
         while(True):
             receivedRaw = self._ser.readline()
             if receivedRaw:
@@ -31,7 +39,8 @@ class SerialReceiver:
 def run():
     """Run main functionality"""
     serialReceiver=SerialReceiver()
-    serialReceiver.connectAndRun(port="/dev/ttyUSB0")
+    serialReceiver.connectAndRun(port="/dev/ttyUSB0",
+                                 zmqPort=22202)
     
 if __name__=="__main__":
     run()
